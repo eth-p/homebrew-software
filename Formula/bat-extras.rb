91da9a97e6a62dfde746f9cafc5619b53e069462
Hosam Aly <hosamaly6@gmail.com>
2020-05-10 11:57:14 +0100

    Bump version to 2020.05.01 and test batdiff

--- Formula/bat-extras.rb
+++ Formula/bat-extras.rb
@@ -1,8 +1,8 @@
 class BatExtras < Formula
   desc "Bash scripts that integrate bat with various command-line tools"
   homepage "https://github.com/eth-p/bat-extras/blob"
-  url "https://github.com/eth-p/bat-extras/archive/v20200408.tar.gz"
-  sha256 "3421eb0d5e4590d8745fc78bdd18c9e66724f47f7b4aef34f826fa83469a51cb"
+  url "https://github.com/eth-p/bat-extras/archive/v2020.05.01.tar.gz"
+  sha256 "ad6a65570f7dfd49e947eec65b2fae52be59feedf7030de801f7fd5b25193885"
   head "https://github.com/eth-p/bat-extras.git"
 
   depends_on "shfmt" => :build
@@ -43,6 +43,9 @@ class BatExtras < Formula
     # batgrep
     assert_match /^batgrep\s+\d{4}\.\d{2}\.\d{2}\b/, shell_output("#{bin}/batgrep --version 2>&1").lines.first.chomp
     assert_equal ["def\n", "fox\n"], shell_output("#{bin}/batgrep '^f|f$' --context=0 --no-color").lines[1..-2]
+
+    # batdiff
+    assert_match /^batdiff\s+\d{4}\.\d{2}\.\d{2}\b/, shell_output("#{bin}/batdiff --version 2>&1").lines.first.chomp
 
     # batman
     assert_match /^batman\s+\d{4}\.\d{2}\.\d{2}\b/, shell_output("#{bin}/batman --version 2>&1").lines.first.chomp
